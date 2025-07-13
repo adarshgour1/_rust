6ee93a16042272fb1e03e78c30e35364e1faa24b
adarshgour1 <adarshgour19@gmail.com>
2025-07-13 12:44:30 +0530

    updated clap

--- xx-clap-learning/src/main.rs
+++ xx-clap-learning/src/main.rs
@@ -1,3 +1,36 @@
+use std::{env::home_dir, fs};
+
+use clap::{Parser, command};
+
+mod github;
+
+#[derive(Debug, Parser)]
+#[command(version, about, long_about= None)]
+struct Cli {
+    /// Github token
+    #[arg(long)]
+    token: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut cli = Cli::parse();
+
+    if let None = cli.token {
+        cli.token = read_gh_token()
+    }
+
+    println!("{:?}", cli);
+}
+
+fn read_gh_token() -> Option<String> {
+    fs::read_to_string(home_dir()?.join(".netrc"))
+        .ok()?
+        .lines()
+        .find_map(|line| {
+            if line.starts_with("  password ") {
+                Some(line.trim_start_matches("  password ").to_owned())
+            } else {
+                None
+            }
+        })
 }
